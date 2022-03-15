b7f0555ce2ce45e3be58fc12dbdc838ba1c1c029
DaniDeniz <dani.deniz@hotmail.com>
2022-03-15 11:59:55 +0100

    Updated repository of Davis Hand Dataset

--- setup_data.sh
+++ setup_data.sh
@@ -1,9 +1,7 @@
 #/bin/bash
-wget https://gist.githubusercontent.com/DaniDeniz/8dadbf249582bae1c570532838592214/raw/5d899ed753e360cd1e3db05094b696252bec0527/download_drive.py
-
 echo "Start downloading Davis Hand Dataset..."
 
-python3 download_drive.py --id 1zuPUSf94wnrlLUfCl5hrVOHhN7AaKbHv --destination ./DavisHandDataset-EventsData.zip
+wget -O DavisHandDataset-EventsData.zip https://drive.ugr.es/index.php/s/WsrU6DVesVdywtn/download
 
 echo "Davis Hand Dataset download finished"
 
